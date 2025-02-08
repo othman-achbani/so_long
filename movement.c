7f75a207b9e0106d660f6ddcbec79bca3607cbe1
othman-achbani <othmanachbani61@gmail.com>
2025-02-09 00:42:13 +0100

    nothing done

--- movement.c
+++ movement.c
@@ -19,20 +19,14 @@ void	move_up(t_map *map)
 
 	y = map->pos_y;
 	x = map->pos_x;
-	// printf("%d | %d \n", map->pos_x , map->pos_y);
-	// printf("good\n");
-	// printf("%d this is y \n", y);
-	// printf("%d this is the map y \n", map->y);
-	// printf("%c this is the map tilesmap \n", map->tilesmap[y-1][x]);
 	if (y < map->y && map->tilesmap[y - 1][x] != '1')
 	{
-		mlx_put_image_to_window(map->mlx , map->win, map->floor,x * PXL, y * PXL);
 		map->tilesmap[y][x] = '0';
 		y--;
 		map->pos_y = y;
 		mlx_put_image_to_window(map->mlx , map->win, map->floor,x * PXL, y * PXL);
-		mlx_put_image_to_window(map->mlx, map->win, map->avatar , x * PXL , y * PXL);
 		map->tilesmap[y][x] = 'P';
+		pass_the_map(map);
 	}
 
 }
